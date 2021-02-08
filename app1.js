9553cc4103ada4d87eabd3753415c597bb3c8581
jcherven <jamiecherven@gmail.com>
2021-02-08 17:04:39 -0500

    added raw lipsum data

--- app1.js
+++ app1.js
@@ -1,2 +1,15 @@
-console.dir(document)
-console.log("yubi yubi")
+const wordNum = document.querySelector('input[name="words"]');
+const paraNum = document.querySelector('input[name="paras"]');
+const btn = document.querySelector('.btn');
+const output = document.querySelector('.output');
+
+btn.addEventListener('click', buttonClicked)
+
+function buttonClicked(e) {
+		console.log(e.target);
+		e.target.style.background = 'blue';
+}
+
+function genParagraphs() {
+
+}
