2cb235fbb5bb78b98b6f3749089fab1a5fa1aa41
jcherven <jamiecherven@gmail.com>
2021-02-08 17:16:00 -0500

    implemented processing of raw text to lipsum bank

--- app1.js
+++ app1.js
@@ -3,6 +3,11 @@ const paraNum = document.querySelector('input[name="paras"]');
 const btn = document.querySelector('.btn');
 const output = document.querySelector('.output');
 
+const regex = /([^A-Za-z ])/g;
+const data = myData.replace(regex, '').toLowerCase();
+
+const myArr = data.split(' ');
+
 btn.addEventListener('click', buttonClicked)
 
 function buttonClicked(e) {
